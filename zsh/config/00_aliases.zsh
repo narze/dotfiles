d0d5f92c50280fc59d03ca0d4836681c57bf5d5b
Manassarn Manoonchai <manassarn@gmail.com>
2017-09-25 23:48:06 +0700

    :wrench: Alias g = git

--- zsh/config/00_aliases.zsh
+++ zsh/config/00_aliases.zsh
@@ -8,6 +8,7 @@ then
 fi
 
 # The rest of my fun git aliases
+alias g='git'
 alias gl='git pull --prune'
 alias gfr='git fetch && git rebase'
 alias glog="git log --graph --pretty=format:'%Cred%h%Creset %an: %s - %Creset %C(yellow)%d%Creset %Cgreen(%cr)%Creset' --abbrev-commit --date=relative"
