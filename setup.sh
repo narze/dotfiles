4e4f91a6b9de6f9b3616929eac23395f23057649
Manassarn Manoonchai <manassarn@gmail.com>
2019-04-30 21:16:59 +0700

    :sparkles: Separate setup into steps

--- setup.sh
+++ setup.sh
@@ -1,39 +1,64 @@
 #!/usr/bin/env bash
 
 ANSIBLE_OPTS="$@"
+ANSIBLE_TAGS=()
+SETUP_BREW=
+SETUP_CASK=
+SETUP_MAS=
+SETUP_ZSH=
+SETUP_MPD=
+SETUP_ASDF=
+SETUP_TMUX=
+SETUP_DOTFILES=
+SETUP_NAS=
 
-q() {
-  QUESTION=$1
-
+_q() {
+  local QUESTION=$1
   local YESNO=""
 
   if [ -n "$ZSH_VERSION" ]; then
-    read -s -k1 "YESNO?$QUESTION [Y/n]"
+    read -s -k1 "YESNO?$QUESTION [Y/n]: "
   elif [ -n "$BASH_VERSION" ]; then
-    read -s -n1 -r -p "$QUESTION [Y/n]" YESNO
+    read -s -n1 -r -p "$QUESTION [Y/n]: " YESNO
   else
     echo "Please use Zsh or Bash"
     exit 1
   fi
 
   if [[ "$YESNO" != "n" ]]; then
-    YESNO=1
+    YESNO="y"
   else
-    YESNO=""
+    YESNO="n"
   fi
 
   echo $YESNO
 }
 
 setup_ask() {
-  local SETUP_FULL=$(q "Full Setup ?")
+  local FULL_SETUP=$(_q "Full Setup ?") ; echo $FULL_SETUP
 
-  echo
-
-  if [[ -n $SETUP_FULL ]]; then
-    echo "Full setup"
+  if [[ $FULL_SETUP == "y" ]]; then
+    ANSIBLE_TAGS+=('all')
   else
-    echo "Selective Setup"
+    SETUP_BREW=$(_q "Install Homebrew packages ?") ; echo $SETUP_BREW
+    SETUP_CASK=$(_q "Install Homebrew Cask packages ?") ; echo $SETUP_CASK
+    SETUP_MAS=$(_q "Install Mas packages?") ; echo $SETUP_MAS
+    SETUP_ZSH=$(_q "Install Zsh with Zplug ?") ; echo $SETUP_ZSH
+    SETUP_MPD=$(_q "Configure mpd ?") ; echo $SETUP_MPD
+    SETUP_ASDF=$(_q "Install asdf with nodejs & yarn ?") ; echo $SETUP_ASDF
+    SETUP_TMUX=$(_q "Configure tmux ?") ; echo $SETUP_TMUX
+    SETUP_DOTFILES=$(_q "Softlink dotfiles ?") ; echo $SETUP_DOTFILES
+    SETUP_NAS=$(_q "Setup your NAS ?") ; echo $SETUP_NAS
+
+    if [[ $SETUP_BREW == "y" ]]; then ANSIBLE_TAGS+=('homebrew'); fi
+    if [[ $SETUP_CASK == "y" ]]; then ANSIBLE_TAGS+=('cask'); fi
+    if [[ $SETUP_MAS == "y" ]]; then ANSIBLE_TAGS+=('mas'); fi
+    if [[ $SETUP_ZSH == "y" ]]; then ANSIBLE_TAGS+=('zsh'); fi
+    if [[ $SETUP_MPD == "y" ]]; then ANSIBLE_TAGS+=('mpd'); fi
+    if [[ $SETUP_ASDF == "y" ]]; then ANSIBLE_TAGS+=('asdf'); fi
+    if [[ $SETUP_TMUX == "y" ]]; then ANSIBLE_TAGS+=('tmux'); fi
+    if [[ $SETUP_DOTFILES == "y" ]]; then ANSIBLE_TAGS+=('dotfiles'); fi
+    if [[ $SETUP_NAS == "y" ]]; then ANSIBLE_TAGS+=('nas'); fi
   fi
 }
 
@@ -79,14 +104,21 @@ encrypt_secrets() {
 }
 
 run_ansible_playbook() {
-  echo "Running Ansible playbook with playbook.yml ..."
-  if ! ansible-playbook ansible/playbook.yml -i ansible/hosts --ask-vault-pass $ANSIBLE_OPTS; then
+  tags=$(IFS=, ; echo "${ANSIBLE_TAGS[*]}")
+  echo "Running Ansible playbook with tags : $tags"
+
+  if ! ansible-playbook ansible/playbook.yml \
+                        -i ansible/hosts \
+                        --ask-vault-pass \
+                        --tags $tags \
+                        $ANSIBLE_OPTS; then
     echo "Error running Ansible playbook"
     exit 1
   fi
 }
 
 setup() {
+  setup_ask
   setup_xcode
   setup_homebrew
   setup_ansible
