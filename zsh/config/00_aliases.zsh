37c1ecf1b96c4652260fdc01bacf77d491303d7f
Manassarn Manoonchai <manassarn@gmail.com>
2019-09-19 02:40:55 +0700

    :sparkles: Add kubectl plugin instead

--- zsh/config/00_aliases.zsh
+++ zsh/config/00_aliases.zsh
@@ -111,17 +111,3 @@ alias d='docker'
 alias dx='docker exec'
 alias ds='docker ps'
 alias dc='docker-compose'
-
-# Commands
-if [ $commands[kubectl] ]; then
-  # Placeholder 'kubectl' shell function:
-  # Will only be executed on the first call to 'kubectl'
-  kubectl() {
-    # Remove this function, subsequent calls will execute 'kubectl' directly
-    unfunction "$0"
-    # Load auto-completion
-    source <(kubectl completion zsh)
-    # Execute 'kubectl' binary
-    $0 "$@"
-  }
-fi
