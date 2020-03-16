fb3ba99400759c9590aa9458fabde379ea20790b
Manassarn Manoonchai <manassarn@gmail.com>
2020-03-16 11:51:22 +0700

    :wrench: Hyper: Add hyper-search plugin

--- etc/hyper.js
+++ etc/hyper.js
@@ -146,7 +146,7 @@ module.exports = {
   //   `hyperpower`
   //   `@company/project`
   //   `project#1.0.1`
-  plugins: ["hyperpower"],
+  plugins: ["hyperpower", "hyper-search"],
 
   // in development, you can create a directory under
   // `~/.hyper_plugins/local/` and include it here
