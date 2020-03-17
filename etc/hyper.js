db85b26dfb3ed3ed58a4be345957ad6617ba3383
Manassarn Manoonchai <manassarn@gmail.com>
2020-03-17 18:19:52 +0700

    :wrench: Change hyper background color

--- etc/hyper.js
+++ etc/hyper.js
@@ -43,7 +43,7 @@ module.exports = {
 
     // terminal background color
     // opacity is only supported on macOS
-    backgroundColor: 'rgba(0,0,0,0.5)',
+    backgroundColor: 'rgba(0,0,0,0.6)',
 
     // terminal selection color
     selectionColor: 'rgba(248,28,229,0.3)',
