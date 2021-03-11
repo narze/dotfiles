23518167ee4f0fae32abbc73f27387439dd7c782
Manassarn Manoonchai <manassarn@gmail.com>
2021-03-11 13:26:39 +0700

    feat(p10k): show node version

--- zsh/p10k.zsh
+++ zsh/p10k.zsh
@@ -59,7 +59,7 @@
     nodenv                  # node.js version from nodenv (https://github.com/nodenv/nodenv)
     nvm                     # node.js version from nvm (https://github.com/nvm-sh/nvm)
     nodeenv                 # node.js environment (https://github.com/ekalinin/nodeenv)
-    # node_version          # node.js version
+    node_version            # node.js version
     # go_version            # go version (https://golang.org)
     # rust_version          # rustc version (https://www.rust-lang.org)
     # dotnet_version        # .NET version (https://dotnet.microsoft.com)
