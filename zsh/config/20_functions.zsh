6e9ba45918eda888f70040ba95b5026860f06cfe
Manassarn Manoonchai <manassarn@gmail.com>
2021-03-24 15:24:57 +0700

    feat(functions): url_encode

--- zsh/config/20_functions.zsh
+++ zsh/config/20_functions.zsh
@@ -93,3 +93,21 @@ function prompt_my_arch_check() {
 music() {
   sh -c "nohup sh -c 'kitty --single-instance --session ~/dotfiles/etc/kitty/sessions/mpd' > /tmp/mpd_nohup.out 2>&1 &"
 }
+
+url_encode() {
+   awk 'BEGIN {
+      for (n = 0; n < 125; n++) {
+         m[sprintf("%c", n)] = n
+      }
+      n = 1
+      while (1) {
+         s = substr(ARGV[1], n, 1)
+         if (s == "") {
+            break
+         }
+         t = s ~ /[[:alnum:]_.!~*\47()-]/ ? t s : t sprintf("%%%02X", m[s])
+         n++
+      }
+      print t
+   }' "$1"
+}
