5c8ab83fe4949e8f52a9c007b49cdb2a8c20c7a6
Manassarn Manoonchai <manassarn@gmail.com>
2021-05-04 19:57:07 +0700

    :sparkles: (functions): Add transfer.sh script

--- zsh/config/20_functions.zsh
+++ zsh/config/20_functions.zsh
@@ -111,3 +111,28 @@ url_encode() {
       print t
    }' "$1"
 }
+
+# Transfer.sh
+# Usage : transfer path/to/file
+transfer() {
+  if [ $# -eq 0 ]; then
+    echo "No arguments specified.\nUsage:\n transfer <file|directory>\n ... | transfer <file_name>" >&2
+    return 1
+  fi
+
+  if tty -s; then
+    file="$1"
+    file_name=$(basename "$file")
+    if [ ! -e "$file" ]; then
+      echo "$file: No such file or directory" >&2
+      return 1
+    fi
+    if [ -d "$file" ]; then
+      file_name="$file_name.zip" ,
+      (cd "$file" && zip -r -q - .) | curl --progress-bar --upload-file "-" "http://transfer.sh/$file_name" | tee /dev/null,
+    else cat "$file" | curl --progress-bar --upload-file "-" "http://transfer.sh/$file_name" | tee /dev/null; fi
+  else
+    file_name=$1
+    curl --progress-bar --upload-file "-" "http://transfer.sh/$file_name" | tee /dev/null
+  fi
+}
