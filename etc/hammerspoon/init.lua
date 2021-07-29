c2e81f9f9eb0b3bbad92051a6882951e3a79d72e
Manassarn Manoonchai <manassarn@gmail.com>
2021-07-29 10:54:22 +0700

    chore(stackline): Use default config

--- etc/hammerspoon/init.lua
+++ etc/hammerspoon/init.lua
@@ -1,18 +1,20 @@
-stackline = require "stackline.stackline.stackline"
+stackline = require "stackline"
+-- stackline = require "stackline.stackline.stackline"
 
-local config = {
-    -- appearance = {
-    --   showIcons = false,       -- default is true
-    -- },
-    features = {
-        clickToFocus = false,  -- default is true
-        -- fzyFrameDetect = {
-        --     fuzzFactor = 25    -- default is 30
-        -- },
-    },
-}
+-- local config = {
+--     -- appearance = {
+--     --   showIcons = false,       -- default is true
+--     -- },
+--     features = {
+--         clickToFocus = false,  -- default is true
+--         -- fzyFrameDetect = {
+--         --     fuzzFactor = 25    -- default is 30
+--         -- },
+--     },
+-- }
 
-stackline:init(config)
+-- stackline:init(config)
+stackline:init()
 
 hs.loadSpoon("FocusHighlight")
 spoon.FocusHighlight:start()
