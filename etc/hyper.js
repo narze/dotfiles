3d29c100e56c4c842c786dbc2c33e0f9dba668f5
Manassarn Manoonchai <manassarn@gmail.com>
2021-10-29 10:44:02 +0700

    Disable hyperpower in hyper

--- etc/hyper.js
+++ etc/hyper.js
@@ -146,7 +146,7 @@ module.exports = {
   //   `hyperpower`
   //   `@company/project`
   //   `project#1.0.1`
-  plugins: ["hyperpower", "hyper-search"],
+  plugins: ["hyper-search"],
 
   // in development, you can create a directory under
   // `~/.hyper_plugins/local/` and include it here
