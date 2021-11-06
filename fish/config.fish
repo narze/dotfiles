167e92b0dff8fd6434e59f750ca010ca59fa2b3d
Manassarn Manoonchai <manassarn@gmail.com>
2021-11-06 21:07:32 +0700

    Add autopair

--- fish/config.fish
+++ fish/config.fish
@@ -1,3 +1,7 @@
+# Paths
+fish_add_path $HOMEBREW_PREFIX/bin
+fish_add_path $HOME/.dotfiles/etc/bin
+
 if status is-interactive
     # Commands to run in interactive sessions can go here
     source ~/.config/fish/abbreviations
