5aa2ceeb1847d14440faf0f848089e6664e8a7e5
Manassarn Manoonchai <manassarn@gmail.com>
2021-11-06 23:14:25 +0700

    Un-ignore fisher

--- fish/config.fish
+++ fish/config.fish
@@ -3,8 +3,8 @@ fish_add_path $HOMEBREW_PREFIX/bin
 fish_add_path $HOME/.dotfiles/etc/bin
 
 if status is-interactive
-    # Commands to run in interactive sessions can go here
-    source ~/.config/fish/abbreviations
+  # Commands to run in interactive sessions can go here
+  source ~/.config/fish/abbreviations
+  zoxide init fish | source
 end
 
-zoxide init fish | source
