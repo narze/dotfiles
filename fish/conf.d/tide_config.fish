72dbc0bec4fa55a643352598aa0406da4abad9df
Manassarn Manoonchai <manassarn@gmail.com>
2021-11-07 15:26:32 +0700

    Fish bg color by day of week

--- fish/conf.d/tide_config.fish
+++ fish/conf.d/tide_config.fish
@@ -1,4 +1,8 @@
-## Arch
+
+# Prompt
+set -U tide_left_prompt_items fish arch pwd git newline character
+
+# Arch
 function _tide_item_arch
   set -l arch (uname -m)
 
@@ -11,12 +15,28 @@ end
 set -U tide_arch_color white
 set -U tide_arch_bg_color black
 
-## Fish
+# Fish
 function _tide_item_fish
   _tide_print_item fish ''
 end
 set -U tide_fish_color black
-set -U tide_fish_bg_color red
 
-## Prompt
-set -U tide_left_prompt_items fish arch pwd git newline character
+# Day color
+switch (date +%a)
+  case Sun
+    set -U tide_fish_bg_color FF5555
+  case Mon
+    set -U tide_fish_bg_color yellow
+  case Tue
+    set -U tide_fish_bg_color FF55FF
+  case Wed
+    set -U tide_fish_bg_color green
+  case Thu
+    set -U tide_fish_bg_color FFA500
+  case Fri
+    set -U tide_fish_bg_color blue
+  case Sat
+    set -U tide_fish_bg_color purple
+  case '*'
+    set -U tide_fish_bg_color white
+end
