7553069a00a93f221356560ad9d1729313e597c6
Manassarn Manoonchai <manassarn@gmail.com>
2021-11-09 10:27:40 +0700

    Setup direnv for fish shell

--- fish/conf.d/asdf.fish
+++ fish/conf.d/asdf.fish
@@ -3,3 +3,6 @@ source ~/.asdf/asdf.fish
 if ! [ -e ~/.config/fish/completions/asdf.fish ]
   mkdir -p ~/.config/fish/completions; and ln -s ~/.asdf/completions/asdf.fish ~/.config/fish/completions
 end
+
+# asdf-direnv hook
+direnv hook fish | source
