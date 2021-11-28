7186851e329ea42d2f28a99c36575480b3aae04f
Manassarn Manoonchai <manassarn@gmail.com>
2021-11-28 23:12:20 +0700

    Install fisher if not exist

--- chezmoi/private_dot_config/fish/config.fish
+++ chezmoi/private_dot_config/fish/config.fish
@@ -1,3 +1,8 @@
+# Install fisher, if not exist
+if status is-interactive && ! functions --query fisher
+  curl -sL https://git.io/fisher | source && fisher update
+end
+
 if status is-interactive
   # Commands to run in interactive sessions can go here
   function fish_greeting
