d24c384040bdf3972aeec6a0e8e09f346c128ef6
Manassarn Manoonchai <manassarn@gmail.com>
2021-11-29 22:41:55 +0700

    Add function to switch shell on odd/even days

--- chezmoi/private_dot_config/fish/conf.d/functions.fish
+++ chezmoi/private_dot_config/fish/conf.d/functions.fish
@@ -1,3 +1,12 @@
 function dir
   basename $PWD
 end
+
+function zsh_odd_fish_even
+  if test (math (date +%d) % 2) -eq 1
+    if command -v zsh >/dev/null 2>&1
+      echo "You should use zsh on odd days!"
+      zsh
+    end
+  end
+end
