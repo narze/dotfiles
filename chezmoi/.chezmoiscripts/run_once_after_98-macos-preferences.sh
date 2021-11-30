4b8539a976b38ab57f3a52973b42632e7b7b85f8
Manassarn Manoonchai <manassarn@gmail.com>
2021-11-30 22:18:26 +0700

    Skip killing apps

--- chezmoi/.chezmoiscripts/run_once_after_98-macos-preferences.sh
+++ chezmoi/.chezmoiscripts/run_once_after_98-macos-preferences.sh
@@ -928,28 +928,29 @@ defaults write com.tapbots.TweetbotMac OpenURLsDirectly -bool true
 # Kill affected applications                                                  #
 ###############################################################################
 
-for app in "Activity Monitor" \
-  "Address Book" \
-  "Calendar" \
-  "cfprefsd" \
-  "Contacts" \
-  "Dock" \
-  "Finder" \
-  "Google Chrome Canary" \
-  "Google Chrome" \
-  "Mail" \
-  "Messages" \
-  "Opera" \
-  "Photos" \
-  "Safari" \
-  "SizeUp" \
-  "Spectacle" \
-  "SystemUIServer" \
-  "Terminal" \
-  "Transmission" \
-  "Tweetbot" \
-  "Twitter" \
-  "iCal"; do
-  killall "${app}" &>/dev/null
-done
+# for app in "Activity Monitor" \
+#   "Address Book" \
+#   "Calendar" \
+#   "cfprefsd" \
+#   "Contacts" \
+#   "Dock" \
+#   "Finder" \
+#   "Google Chrome Canary" \
+#   "Google Chrome" \
+#   "Mail" \
+#   "Messages" \
+#   "Opera" \
+#   "Photos" \
+#   "Safari" \
+#   "SizeUp" \
+#   "Spectacle" \
+#   "SystemUIServer" \
+#   "Terminal" \
+#   "Transmission" \
+#   "Tweetbot" \
+#   "Twitter" \
+#   "iCal"; do
+#   killall "${app}" &>/dev/null
+# done
+
 echo "Done. Note that some of these changes require a logout/restart to take effect."
