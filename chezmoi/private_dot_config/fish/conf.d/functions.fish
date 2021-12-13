c4ff56597ef8c7faee2922cd9cdbe51ff13d8383
Manassarn Manoonchai <manassarn@gmail.com>
2021-12-13 10:35:02 +0700

    Remove zsh_odd_fish_even

--- chezmoi/private_dot_config/fish/conf.d/functions.fish
+++ chezmoi/private_dot_config/fish/conf.d/functions.fish
@@ -1,12 +1,3 @@
 function dir
   basename $PWD
 end
-
-function zsh_odd_fish_even
-  if test (math (date +%d) % 2) -eq 1
-    if command -v zsh >/dev/null 2>&1
-      echo "You should use zsh on odd days!"
-      zsh
-    end
-  end
-end
