a27c036ea11ec2b45d98a60b41a78022b17912fe
Manassarn Manoonchai <manassarn@gmail.com>
2022-04-05 22:54:18 +0700

    chore: config FocusHighlight

--- chezmoi/dot_hammerspoon/init.lua
+++ chezmoi/dot_hammerspoon/init.lua
@@ -18,5 +18,11 @@ stackline:init()
 
 hs.loadSpoon("FocusHighlight")
 spoon.FocusHighlight:start()
+spoon.FocusHighlight.color = "#ffffff"
+spoon.FocusHighlight.windowFilter = hs.window.filter.default
+spoon.FocusHighlight.arrowSize = 0
+spoon.FocusHighlight.arrowFadeOutDuration = 0
+spoon.FocusHighlight.highlightFadeOutDuration = 0.3
+spoon.FocusHighlight.highlightFillAlpha = 0.03
 
 require "wm"
