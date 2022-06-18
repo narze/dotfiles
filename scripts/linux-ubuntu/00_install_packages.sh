e533a3d0c56ebe718747226169079a3cd30258e8
Manassarn Manoonchai <manassarn@gmail.com>
2022-06-18 10:26:15 +0000

    feat(linux): replace if already installed

--- scripts/linux-ubuntu/00_install_packages.sh
+++ scripts/linux-ubuntu/00_install_packages.sh
@@ -2,7 +2,7 @@
 
 apt update
 
-apt install -y zsh
+apt install -y unzip zsh
 
 # Dependencies for asdf
 apt install -y curl git
--- scripts/linux-ubuntu/10_exa.sh
+++ scripts/linux-ubuntu/10_exa.sh
@@ -1,9 +1,7 @@
 #!/bin/sh
 
-apt install -y unzip
-
 EXA_VERSION=$(curl -s "https://api.github.com/repos/ogham/exa/releases/latest" | grep -Po '"tag_name": "v\K[0-9.]+')
 
 curl -Lo /tmp/exa.zip "https://github.com/ogham/exa/releases/latest/download/exa-linux-x86_64-v${EXA_VERSION}.zip"
 
-sudo unzip -q /tmp/exa.zip bin/exa -d /usr/local
+sudo unzip -o -q /tmp/exa.zip bin/exa -d /usr/local
