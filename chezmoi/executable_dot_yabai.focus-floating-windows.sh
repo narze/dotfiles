fb00d7786db55cd16120f0a24f0a52ba1c437ee8
Manassarn Manoonchai <manassarn@gmail.com>
2022-07-13 13:15:25 +0700

    fix: disable focus float script temporarily

--- chezmoi/executable_dot_yabai.focus-floating-windows.sh
+++ chezmoi/executable_dot_yabai.focus-floating-windows.sh
@@ -5,6 +5,9 @@ window=$(yabai -m query --windows --window "$window_id")
 window_subrole=$(echo "$window" | jq -r '.subrole')
 window_is_floating=$(echo "$window" | jq -r '."is-floating"')
 
+# TODO: Don't focus ignored apps
+# yabai -m rule --list | jq -r ".[] | select(.manage == false) | .app"
+
 # echo "$window_id" >>/tmp/yabai.txt
 # echo "$window_subrole" >>/tmp/yabai.txt
 # echo "$window_is_floating" >>/tmp/yabai.txt
@@ -12,9 +15,9 @@ window_is_floating=$(echo "$window" | jq -r '."is-floating"')
 if [ "$window_subrole" = "AXDialog" ]; then
   if [ "$window_is_floating" = "false" ]; then
     yabai -m window --toggle float --window "$window_id"
-    # echo toggle float >>/tmp/yabai.txt
+    echo toggle float >>/tmp/yabai.txt
   fi
 
-  # echo toggle focus $window_id >>/tmp/yabai.txt
+  echo toggle focus $window_id >>/tmp/yabai.txt
   yabai -m window --focus "$window_id"
 fi
