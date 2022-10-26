cab8034f135b163faff5515fbcf0bd45ef769d03
Manassarn Manoonchai <manassarn@gmail.com>
2022-10-26 10:27:48 +0700

    Don't manage Arc PiP window

--- chezmoi/executable_dot_yabai.focus-standard-window.sh
+++ chezmoi/executable_dot_yabai.focus-standard-window.sh
@@ -5,6 +5,9 @@ window=$(yabai -m query --windows --window "$window_id")
 window_subrole=$(echo "$window" | jq -r '.subrole')
 
 if [ "$window_subrole" = "AXStandardWindow" ]; then
-  echo focus standard window $window_id >>/tmp/yabai.txt
-  yabai -m window --focus "$window_id"
+  # If window is not named Picture in Picture
+  if [ "$(echo "$window" | jq -r '.title')" != "Picture in Picture" ]; then
+    echo focus standard window $window_id >>/tmp/yabai.txt
+    yabai -m window --focus "$window_id"
+  fi
 fi
