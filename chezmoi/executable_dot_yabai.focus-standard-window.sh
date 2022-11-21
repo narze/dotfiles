bdb6f83c576d0fbf4ef42024622289262dc72c42
Manassarn Manoonchai <manassarn@gmail.com>
2022-11-21 15:53:47 +0700

    Ignore Fig Autocomplete

--- chezmoi/executable_dot_yabai.focus-standard-window.sh
+++ chezmoi/executable_dot_yabai.focus-standard-window.sh
@@ -5,9 +5,11 @@ window=$(yabai -m query --windows --window "$window_id")
 window_subrole=$(echo "$window" | jq -r '.subrole')
 
 if [ "$window_subrole" = "AXStandardWindow" ]; then
-  # If window is not named Picture in Picture
+  # TODO: exclude "manage=off" apps
   if [ "$(echo "$window" | jq -r '.title')" != "Picture in Picture" ]; then
-    echo focus standard window $window_id >>/tmp/yabai.txt
-    yabai -m window --focus "$window_id"
+    if [ "$(echo "$window" | jq -r '.title')" != "Fig Autocomplete" ]; then
+      echo focus standard window $window_id >>/tmp/yabai.txt
+      yabai -m window --focus "$window_id"
+    fi
   fi
 fi
