70d275479581437ee70defd54d8cf4df84dcd992
Manassarn Manoonchai <manassarn@gmail.com>
2024-01-03 18:54:54 +0700

    Modify spotify indicator

--- chezmoi/private_dot_config/sketchybar/plugins/executable_spotifyIndicator.sh
+++ chezmoi/private_dot_config/sketchybar/plugins/executable_spotifyIndicator.sh
@@ -9,15 +9,22 @@ ALBUM=""
 ARTIST=""
 if [ "$(osascript -e 'if application "Spotify" is running then tell application "Spotify" to get player state')" == "playing" ]; then
   PLAYING=0
-  TRACK=$(osascript -e 'tell application "Spotify" to get name of current track')
-  ARTIST=$(osascript -e 'tell application "Spotify" to get artist of current track')
-  ALBUM=$(osascript -e 'tell application "Spotify" to get album of current track')
 fi
+TRACK=$(osascript -e 'tell application "Spotify" to get name of current track')
+ARTIST=$(osascript -e 'tell application "Spotify" to get artist of current track')
+ALBUM=$(osascript -e 'tell application "Spotify" to get album of current track')
+
 if [ $RUNNING -eq 0 ] && [ $PLAYING -eq 0 ]; then
   if [ "$ARTIST" == "" ]; then
-    sketchybar -m --set $NAME label=" $TRACK  $ALBUM"
+    sketchybar -m --set $NAME label="$TRACK  $ALBUM" icon.font="sketchybar-app-font:Regular:13" icon=":spotify:"
   else
-    sketchybar -m --set $NAME label=" $TRACK  $ARTIST"
+    sketchybar -m --set $NAME label="$TRACK  $ARTIST" icon.font="sketchybar-app-font:Regular:13" icon=":spotify:"
+  fi
+elif [ $RUNNING -eq 0 ] && [ $PLAYING -eq 1 ]; then
+  if [ "$ARTIST" == "" ]; then
+    sketchybar -m --set $NAME label="$TRACK  $ALBUM" icon.font="sketchybar-app-font:Regular:13" icon=":paused:"
+  else
+    sketchybar -m --set $NAME label="$TRACK  $ARTIST" icon.font="sketchybar-app-font:Regular:13" icon=":paused:"
   fi
 else
   sketchybar -m --set $NAME drawing="off"
