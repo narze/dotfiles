688a0833c17c95f1930d14d0897875c650d02403
Manassarn Manoonchai <manassarn@gmail.com>
2024-01-04 02:21:10 +0700

    Optimize

--- chezmoi/executable_dot_yabai.swap-largest.sh
+++ chezmoi/executable_dot_yabai.swap-largest.sh
@@ -2,11 +2,14 @@
 
 current_id=$(yabai -m query --windows --window | jq .id)
 largest_id=$(yabai -m query --windows --window largest | jq .id)
-recent_id=$(yabai -m query --windows --window recent | jq .id)
+# recent_id=$(yabai -m query --windows --window recent | jq .id)
 
 if [ "$current_id" = "$largest_id" ]; then
-  yabai -m window --swap recent
-  yabai -m window --focus "$recent_id"
+  yabai -m window --focus recent
+  yabai -m window --swap "$current_id"
+
+  # yabai -m window --swap recent
+  # yabai -m window --focus "$recent_id"
 else
   yabai -m window --focus largest
   yabai -m window --swap "$current_id"
