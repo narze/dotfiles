a1626286d09be0ade3c2edb512991ecce6418843
Manassarn Manoonchai <manassarn@gmail.com>
2024-01-04 00:31:08 +0700

    Sketchybar - Wifi

---
+++ chezmoi/private_dot_config/sketchybar/plugins/executable_wifi.sh
@@ -0,0 +1,11 @@
+#!/usr/bin/env sh
+
+CURRENT_WIFI="$(/System/Library/PrivateFrameworks/Apple80211.framework/Versions/Current/Resources/airport -I)"
+SSID="$(echo "$CURRENT_WIFI" | grep -o "SSID: .*" | sed 's/^SSID: //')"
+CURR_TX="$(echo "$CURRENT_WIFI" | grep -o "lastTxRate: .*" | sed 's/^lastTxRate: //')"
+
+if [ "$SSID" = "" ]; then
+  sketchybar --set $NAME label="Disconnected" icon=󰖪 label.font.size=10
+else
+  sketchybar --set $NAME label="$SSID (${CURR_TX}M)" icon= label.font.size=10
+fi
