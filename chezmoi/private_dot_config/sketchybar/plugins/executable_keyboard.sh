cdaf0914fb3acd5d27d8f2095d2f99a41314092a
Manassarn Manoonchai <manassarn@gmail.com>
2024-12-06 22:35:39 +0700

    Fix keyboard layout flag not showing 🇯🇵

--- chezmoi/private_dot_config/sketchybar/plugins/executable_keyboard.sh
+++ chezmoi/private_dot_config/sketchybar/plugins/executable_keyboard.sh
@@ -1,15 +1,35 @@
 #!/bin/bash
 
-# this is jank and ugly, I know
-LAYOUT="$(defaults read ~/Library/Preferences/com.apple.HIToolbox.plist AppleSelectedInputSources | grep "KeyboardLayout Name" | cut -c 33- | rev | cut -c 2- | rev)"
+#!/bin/bash
+
+# Read the input sources
+input_sources=$(defaults read ~/Library/Preferences/com.apple.HIToolbox.plist AppleSelectedInputSources)
+layout_name=$(echo "$input_sources" | grep -oE '"KeyboardLayout Name" = [^;]+' | awk -F'= ' '{print $2}' | tr -d '"')
+bundle_id=$(echo "$input_sources" | grep -oE '"Bundle ID" = [^;]+' | awk -F'= ' '{print $2}' | tr -d '"')
+
+if [[ -n $layout_name ]]; then
+  echo "Current Keyboard Layout: $layout_name"
+elif [[ -n $bundle_id ]]; then
+  echo "Current Input Source: $bundle_id"
+else
+  echo "No keyboard layout or input source detected."
+fi
 
 # specify short layouts individually.
-case "$LAYOUT" in
-    "Colemak") SHORT_LAYOUT="🇺🇸";;
-    "\"Colemak DHm\"") SHORT_LAYOUT="🇺🇸";;
-    "\"U.S.\"") SHORT_LAYOUT="🇺🇸";;
-    "Manoonchai") SHORT_LAYOUT="🇹🇭";;
-    *) SHORT_LAYOUT="?";;
+case "$layout_name" in
+  "Colemak") SHORT_LAYOUT="🇺🇸";;
+  "Colemak DHm") SHORT_LAYOUT="🇺🇸";;
+  "U.S.") SHORT_LAYOUT="🇺🇸";;
+  "Manoonchai") SHORT_LAYOUT="🇹🇭";;
+  *) SHORT_LAYOUT="?";;
 esac
+
+# If layout_name is empty, then check if bundle_id includes Japanese, then set the flag to 🇯🇵
+if [[ -z $layout_name ]]; then
+  case "$bundle_id" in
+    *"Japanese"*) SHORT_LAYOUT="🇯🇵";;
+    *) SHORT_LAYOUT="?";;
+  esac
+fi
 
 sketchybar --set keyboard label="$SHORT_LAYOUT" icon.drawing="off" label.padding_left=6 label.padding_right=6
