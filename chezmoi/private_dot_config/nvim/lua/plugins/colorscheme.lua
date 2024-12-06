67c7cd2b61d530279b2594071069971e47106030
Manassarn Manoonchai <manassarn@gmail.com>
2024-12-07 01:39:14 +0700

    Set colorscheme to tokyonight-night

---
+++ chezmoi/private_dot_config/nvim/lua/plugins/colorscheme.lua
@@ -0,0 +1,14 @@
+return {
+  {
+    "folke/tokyonight.nvim",
+    lazy = true,
+    opts = { style = "night" },
+  },
+
+  {
+    "LazyVim/LazyVim",
+    opts = {
+      colorscheme = "tokyonight",
+    },
+  },
+}
