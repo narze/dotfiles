e41bee2af7d5aeb7d15250161eb4e876940b9baf
Manassarn Manoonchai <manassarn@gmail.com>
2024-12-08 23:05:01 +0700

    Config Neo-tree

--- chezmoi/private_dot_config/nvim/lua/config/keymaps.lua
+++ chezmoi/private_dot_config/nvim/lua/config/keymaps.lua
@@ -1,3 +1,8 @@
 -- Keymaps are automatically loaded on the VeryLazy event
 -- Default keymaps that are always set: https://github.com/LazyVim/LazyVim/blob/main/lua/lazyvim/config/keymaps.lua
 -- Add any additional keymaps here
+
+local map = vim.keymap.set
+
+-- Cmd + Enter = My custom keymap group
+map("n", "<leader><enter>b", "<Cmd>Neotree toggle<CR>", { desc = "NeoTree toggle" })
---
+++ chezmoi/private_dot_config/nvim/lua/plugins/which_key.lua
@@ -0,0 +1,16 @@
+return {
+  {
+    "folke/which-key.nvim",
+    event = "VeryLazy",
+    opts_extend = { "spec" },
+    opts = {
+      defaults = {},
+      spec = {
+        {
+          mode = { "n", "v" },
+          { "<leader><enter>", group = "My Commands" },
+        },
+      },
+    },
+  },
+}
