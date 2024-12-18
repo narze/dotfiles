feb85402585f2d961b2ad980229650af23725756
Manassarn Manoonchai <manassarn@gmail.com>
2024-12-18 11:31:22 +0700

    Add Raycast script

---
+++ chezmoi/private_dot_config/raycast/scripts/executable_restart_aerospace.rb
@@ -0,0 +1,23 @@
+#!/usr/bin/env ruby
+
+# Required parameters:
+# @raycast.schemaVersion 1
+# @raycast.title Restart Aerospace
+# @raycast.mode compact
+
+# Optional parameters:
+# @raycast.icon 🤖
+
+# Documentation:
+# @raycast.author narze
+
+# Quit AeroSpace.app and start again
+system('pkill -9 AeroSpace')
+sleep 0.5
+system('open -a AeroSpace')
+
+# Also restart sketchybar
+system('brew services restart sketchybar')
+
+# Notify macOS that the app is restarted
+system("osascript -e 'display notification \"AeroSpace restarted\" with title \"AeroSpace\"'")
