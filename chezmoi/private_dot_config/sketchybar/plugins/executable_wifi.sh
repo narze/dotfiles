e79a64937a51c4437b1239ba42d0d08def7bb107
Manassarn Manoonchai <manassarn@gmail.com>
2025-01-17 16:38:06 +0700

    Fix wifi ssid name

--- chezmoi/private_dot_config/sketchybar/plugins/executable_wifi.sh
+++ chezmoi/private_dot_config/sketchybar/plugins/executable_wifi.sh
@@ -1,11 +1,14 @@
 #!/usr/bin/env sh
 
-CURRENT_WIFI="$(/System/Library/PrivateFrameworks/Apple80211.framework/Versions/Current/Resources/airport -I)"
-SSID="$(echo "$CURRENT_WIFI" | grep -o "SSID: .*" | sed 's/^SSID: //')"
-CURR_TX="$(echo "$CURRENT_WIFI" | grep -o "lastTxRate: .*" | sed 's/^lastTxRate: //')"
+# Stops working on Sonoma
+# CURRENT_WIFI="$(/System/Library/PrivateFrameworks/Apple80211.framework/Versions/Current/Resources/airport -I)"
+# SSID="$(echo "$CURRENT_WIFI" | grep -o "SSID: .*" | sed 's/^SSID: //')"
+# CURR_TX="$(echo "$CURRENT_WIFI" | grep -o "lastTxRate: .*" | sed 's/^lastTxRate: //')"
+
+SSID="$(ipconfig getsummary "$(networksetup -listallhardwareports | awk '/Hardware Port: Wi-Fi/{getline; print $2}')" | awk -F ' SSID : ' '/ SSID : / {print $2}')"
 
 if [ "$SSID" = "" ]; then
-  sketchybar --set $NAME label="Disconnected" icon=󰖪 label.font.size=10
+  sketchybar --set "$NAME" label="Disconnected" icon=󰖪 label.font.size=10
 else
-  sketchybar --set $NAME label="$SSID (${CURR_TX}M)" icon= label.font.size=10
+  sketchybar --set "$NAME" label="$SSID" icon= label.font.size=10
 fi
