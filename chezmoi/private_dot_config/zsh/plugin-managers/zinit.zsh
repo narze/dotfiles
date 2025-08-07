edd98bdb7dfa7194132e4f6cdbc021bf59fa9a4b
Manassarn Manoonchai <manassarn@gmail.com>
2025-08-08 02:27:14 +0700

    Evaluate zim & zinit, remove z4h

---
+++ chezmoi/private_dot_config/zsh/plugin-managers/zinit.zsh
@@ -0,0 +1,41 @@
+### Added by Zinit's installer
+if [[ ! -f $HOME/.local/share/zinit/zinit.git/zinit.zsh ]]; then
+    print -P "%F{33} %F{220}Installing %F{33}ZDHARMA-CONTINUUM%F{220} Initiative Plugin Manager (%F{33}zdharma-continuum/zinit%F{220})…%f"
+    command mkdir -p "$HOME/.local/share/zinit" && command chmod g-rwX "$HOME/.local/share/zinit"
+    command git clone https://github.com/zdharma-continuum/zinit "$HOME/.local/share/zinit/zinit.git" && \
+        print -P "%F{33} %F{34}Installation successful.%f%b" || \
+        print -P "%F{160} The clone has failed.%f%b"
+fi
+
+source "$HOME/.local/share/zinit/zinit.git/zinit.zsh"
+
+autoload -Uz _zinit
+(( ${+_comps} )) && _comps[zinit]=_zinit
+
+# Load a few important annexes, without Turbo
+# (this is currently required for annexes)
+zinit light-mode for \
+    zdharma-continuum/zinit-annex-as-monitor \
+    zdharma-continuum/zinit-annex-bin-gem-node \
+    zdharma-continuum/zinit-annex-patch-dl \
+    zdharma-continuum/zinit-annex-rust
+
+### End of Zinit's installer chunk
+
+# Load starship theme
+# line 1: `starship` binary as command, from github release
+# line 2: starship setup at clone(create init.zsh, completion)
+# line 3: pull behavior same as clone, source init.zsh
+zinit ice as"command" from"gh-r" \
+          atclone"./starship init zsh > init.zsh; ./starship completions zsh > _starship" \
+          atpull"%atclone" src"init.zsh"
+zinit light starship/starship
+
+zinit light romkatv/zsh-defer
+# zinit ice wait"0" silent
+zsh-defer zinit light olets/zsh-abbr
+zsh-defer zinit light mollifier/cd-gitroot
+
+zsh-defer zinit light zsh-users/zsh-syntax-highlighting
+zsh-defer zinit light zsh-users/zsh-history-substring-search
+zsh-defer zinit light zsh-users/zsh-autosuggestions
