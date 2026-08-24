3d83b855611a68962a40834903a79e69169b9d05
Manassarn Manoonchai <manassarn@gmail.com>
2026-08-24 10:31:16 +0700

    nvim: keep d/D from overwriting the system clipboard
    Only sync actual yanks to the macOS clipboard via TextYankPost;
    LazyVim's default unnamedplus synced delete/change too.

--- chezmoi/private_dot_config/nvim/lua/config/autocmds.lua
+++ chezmoi/private_dot_config/nvim/lua/config/autocmds.lua
@@ -1,3 +1,15 @@
 -- Autocmds are automatically loaded on the VeryLazy event
 -- Default autocmds that are always set: https://github.com/LazyVim/LazyVim/blob/main/lua/lazyvim/config/autocmds.lua
 -- Add any additional autocmds here
+
+-- Sync yanked text to the system clipboard, but not deleted/changed text.
+-- Pairs with `vim.opt.clipboard = ""` in options.lua.
+vim.api.nvim_create_autocmd("TextYankPost", {
+  desc = "Sync yank (not delete/change) to the system clipboard",
+  group = vim.api.nvim_create_augroup("sync_yank_to_clipboard", { clear = true }),
+  callback = function()
+    if vim.v.event.operator == "y" then
+      vim.fn.setreg("+", vim.v.event.regcontents)
+    end
+  end,
+})
--- chezmoi/private_dot_config/nvim/lua/config/options.lua
+++ chezmoi/private_dot_config/nvim/lua/config/options.lua
@@ -1,3 +1,9 @@
 -- Options are automatically loaded before lazy.nvim startup
 -- Default options that are always set: https://github.com/LazyVim/LazyVim/blob/main/lua/lazyvim/config/options.lua
 -- Add any additional options here
+
+-- LazyVim defaults to "unnamedplus", which syncs the system clipboard on
+-- every yank AND delete/change. Turn that off here; a TextYankPost autocmd
+-- in autocmds.lua syncs only actual yanks to the clipboard instead, so d/D
+-- (and c/x) no longer clobber it.
+vim.opt.clipboard = ""
